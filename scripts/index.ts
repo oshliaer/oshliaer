ffc6bafbf1edc6a1c4df66c65c7971ff15bf484e
Alexander Ivanov <oshli.a.er@gmail.com>
2024-02-02 22:49:34 +0300

    feat: add template script
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

---
+++ scripts/index.ts
@@ -0,0 +1,46 @@
+import path from 'path';
+import * as fs from 'fs';
+import * as fsp from 'fs/promises';
+import rec from 'recursive-readdir';
+import mustashe from 'mustache';
+
+const PATH_ = './docs';
+
+const dictStats: Record<string, fs.Stats> = {};
+
+const ignore = (file: string, stat: fs.Stats) => {
+  dictStats[file] = stat;
+  return !stat.isDirectory() && path.extname(file) !== '.md';
+};
+
+async function run() {
+  const list = await rec(PATH_, [ignore]);
+
+  const lastItems = list
+    .map((str: string) => {
+      const parts = str.split('/');
+      const index = parts.indexOf('docs');
+      const link = parts.slice(index).join('/');
+      const title = parts
+        .slice(index + 1)
+        .map((s) => s.replace(/\.md$/, ''))
+        .join(' => ');
+      console.log(dictStats[str]);
+      return {
+        parts,
+        title,
+        link,
+        stat: dictStats[str],
+      };
+    })
+    .map((part) => `- [${part.title}](${part.link})`)
+    .join('\n');
+
+  const tempalte = await fsp.readFile(path.join('./scripts/templates/readme.md'), 'utf8');
+
+  const doc = mustashe.render(tempalte, { lastItems });
+
+  fsp.writeFile(path.join('./readme.md'), doc, 'utf8');
+}
+
+run();
---
+++ scripts/templates/readme.md
@@ -0,0 +1,13 @@
+## Hi there!
+
+Last updated: 2022-03-18 15:23:06
+
+![Sheets Edit Event](https://en1t1jt2c6ghd7u.m.pipedream.net)
+
+<!-- toc -->
+
+<!-- tocstop -->
+
+### Last items
+
+{{lastItems}}
