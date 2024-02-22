383c3678ed8c44d91ba5f5c241cac3f8a46e4e81
Alexander Ivanov <oshli.a.er@gmail.com>
2024-02-22 15:57:35 +0300

    feat: add dowload chrome
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

---
+++ actions/shell/install-chrome.sh
@@ -0,0 +1,2 @@
+curl -o /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
